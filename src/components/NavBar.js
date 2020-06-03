e678c0caa399284fe6865135c38cd6ebff1c2ea6
rDionysus <dcho@receptiviti.com>
2020-06-03 13:19:09 -0400

    init commit. materialUI and auth0 loaded

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+// src/components/NavBar.js
+
+import React from "react";
+import { useAuth0 } from "../react-auth0-spa";
+import Button from '@material-ui/core/Button';
+
+const NavBar = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <div>
+      <Button color = "primary">Solutions</Button>
+      <Button color = "primary">Resources</Button>
+      <Button color = "primary">Pricing</Button>
+      <Button color = "primary">Why?</Button>
+
+      {!isAuthenticated && (
+        <Button variant="outlined" color="primary" onClick={() => loginWithRedirect({})}>Log in</Button>
+      )}
+
+      {isAuthenticated && <Button variant="outlined" color="secondary" onClick={() => logout()}>Log out</Button>}
+    </div>
+  );
+};
+
+export default NavBar;
