6e5b8206e2ab817ef1873b63b278a1230c506b2d
rDionysus <dcho@receptiviti.com>
2020-06-03 15:29:15 -0400

    added profile info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 // src/App.js
-
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import PrivateRoute from "./components/PrivateRoute";
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,14 +2,22 @@
 
 import React from "react";
 import { useAuth0 } from "../react-auth0-spa";
+// import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 import './NavBar.css';
 
 const NavBar = () => {
-  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, loading, user } = useAuth0();
+
+  if (loading) {
+    return <div></div>;
+  }
 
   return (
     <div id="navbar">
+      
+      <Button color = "primary">*Receptiviti</Button>
       <Button color = "primary">Solutions</Button>
       <Button color = "primary">Resources</Button>
       <Button color = "primary">Pricing</Button>
@@ -19,8 +27,11 @@ const NavBar = () => {
         <Button variant="outlined" color="primary" onClick={() => loginWithRedirect({})}>Log in</Button>
       )}
 
-      {isAuthenticated && <Button variant="outlined" color="secondary" onClick={() => logout()}>Log out</Button>}
-    </div>
+      {isAuthenticated && (<Avatar alt={user.name} src={user.picture} />)}
+      {/* {isAuthenticated && (<Button color = "primary" to="/profile">Profile</Button>)} */}
+      {isAuthenticated && (<Button variant="outlined" color="secondary" onClick={() => logout()}>Log out</Button>)}
+
+      </div>
   );
 };
 
